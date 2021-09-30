6d103bc3b43096523f45922b8b8de3c7ad10cd81
Ri-dha <tareqridha@gmail.com>
2021-10-01 00:08:34 +0300

    sloution for js task

--- task.js
+++ task.js
@@ -1,44 +1,65 @@
-let numbers = [
-    5,
-    8,
-    0,
-    1,
-    9,
-    11,
-    15,
-    16
-];
+//to make the array immutable we add const
 
-console.log("Original numbers list: ", numbers)
 
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
+const numbers = [5,8,0,1,9,11,15,16];
+
+
+
+
+//  Reading the data.txt file 
+
+var fs = require ('fs')
+var txt = fs.readFileSync('data.txt', 'utf8')
+
+
+// storing the file data.txt in an array
+
+let dataArray=[]
+
+
+// turning the data array to an array of numbers 
+dataArray=txt.split(',')
+dataArray= dataArray.map(elem=> parseInt(elem, 10))
+
+
+// making the numbers asceind 
+function asc(ar){
+    for(let i = 0 ; i < ar.length - 1  ; i++)
     {
-        if(numbers[i] > numbers[j + 1])
+        for(let j = 0 ; j < ar.length - 1 - i ; j++)
         {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
+            if(ar[j] > ar[j + 1])
+            {
+                [ar[j], ar[j+1]] = [ar[j+1], ar[j]]  
+
+            }
         }
     }
+
+console.log("Numbers list After Asc sorting: ", ar)
 }
 
-console.log("Numbers list After sorting: ", numbers)
 
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
+// making the numbers descend
+function desc(arr){
+    for(let i = 0 ; i < arr.length ; i++)
     {
-        if(numbers[i] < numbers[j + 1])
+        for(let j = 0 ; j < arr.length - 1 ; j++)
         {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
+            if(arr[j] < arr[j + 1])
+            {
+          [arr[j], arr[j+1]] = [arr[j+1], arr[j]]  
+         }
         }
     }
+    
+    console.log("Numbers list After Desc sorting: ", arr)
 }
 
-console.log("Numbers list After Desc sorting: ", numbers)
 
+console.log("immutable numbers list: ", numbers)
 
+// sort the numbers in ascending order, then store it in a new file called output.txt
+
+asc(dataArray)
+fs.writeFileSync('OutPut.txt', dataArray.toString());
